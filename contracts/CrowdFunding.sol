3296e605846651e0c74b924b378a2cb6c21141d2
VitorOdorico <ve743137@gmail.com>
2023-01-09 11:25:44 -0300

    start project and config to solidity]

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaing{
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+    
+    mapping(uint256 => Campaing) public campaigns;
+
+    uint256 public numberOfCampaings = 0;
+    
+    function createCampaign(address _owner,string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image)public returns(uint256){
+
+        Campaing storage campaign = campaigns[numberOfCampaings];
+
+        require(campaign.deadline < block.timestamp, "the deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+    
+        numberOfCampaings++;
+
+        return numberOfCampaings -1;
+    }
+
+    function donateToCampaign(uint256 _id){}
+
+    function getDonators(){}
+
+    function getCampaigns(){}
+
+}
